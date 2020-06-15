a361451718a7dc445e2fa5ee22b283aa28a72bbf
ABHISHEK VISHWAS KADAM <41193151+abhishekkadam-gep@users.noreply.github.com>
2020-06-15 18:16:48 +0530

    premia-12-6-2020

--- src/app/admissions/admissions.component.ts
+++ src/app/admissions/admissions.component.ts
@@ -8,6 +8,51 @@ import { Component, OnInit } from '@angular/core';
 export class AdmissionsComponent implements OnInit {
 
   constructor() { }
+  
+  admission = [
+    {
+      icon: '#icon_softEngg',
+      description: `Ethical
+      Education`,
+    },
+    {
+      icon: '#icon_softEngg',
+      description: `Nurturing
+      Individual
+      Passion`,
+    },
+    {
+      icon: '#icon_softEngg',
+      description: `Building
+A Community`,
+    }
+  ];
+
+  admission_2 = [
+    {
+      title: 'Admission to PP 1',
+      bar:'pp1',
+      description: `The child needs to be 3 
+years 6 months as on
+1st June 2020`,
+    },
+    {
+      title: 'Admission to PP 2',
+      bar:'pp2',
+      description: `The child needs to be 4
+years 6 months as on 1st 
+June 2020`,
+    },
+    {
+      title: 'Grade 1',
+      bar:'pp3',
+      description: `The child needs to be 5 
+years 6 months as on 1st 
+June 2020`,
+    }
+  ];
+
+
 
   ngOnInit() {
   }
--- src/app/apply/apply.component.ts
+++ src/app/apply/apply.component.ts
@@ -8,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class ApplyComponent implements OnInit {
 
   constructor() { }
-
+  moreInfo = [
+    { info : 'Please fill out and submit the Inquiry Form on our website'},
+    { info : 'Visit our school campus or Contact our Admissions office'},
+    { info : 'Complete and submit the Application for Admission Form when you visit our school.'},
+    { info : ' Once you have completed the application, we will schedule a brief parent interview and an assessment for your child '},
+    { info : ' Once accepted, we will complete the admissions and payment procedure'}
+  ];
   ngOnInit() {
   }
 
