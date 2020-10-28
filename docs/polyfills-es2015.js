4d71d255cf9c86a4c86526ae827c204c2d984627
ABHISHEK VISHWAS KADAM <41193151+abhishekkadam-gep@users.noreply.github.com>
2020-10-28 10:04:39 +0530

    tpa-28-10-20-adhoc

--- docs/polyfills-es2015.js
+++ docs/polyfills-es2015.js
@@ -6491,8 +6491,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! F:\github\academy\src\polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! F:\github\academy\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! D:\academy\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! D:\academy\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
